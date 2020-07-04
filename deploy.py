7af99bcfa7bf02378f651ed4a8e8e793abb09c3f
Kiprono Elijah Koech <kiprono@aims.ac.za>
2020-07-04 21:06:46 +0200

    last touch on deploy.py

--- deploy.py
+++ deploy.py
@@ -3,14 +3,29 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from scraper2 import Scraper
+import numpy as np
 
+
+#To understand the basics go to: https://gspread.readthedocs.io/en/latest/
+
+# Defining the scope of file interaction
 scope = (["https://spreadsheets.google.com/feeds",
           "https://www.googleapis.com/auth/spreadsheets",
           "https://www.googleapis.com/auth/drive.file",
           "https://www.googleapis.com/auth/drive"])
+#loading the credentials file
 creds =  ServiceAccountCredentials.from_json_keyfile_name("./assets/api.json",scope)
 client = gspread.authorize(creds)
-sheet = client.open("data").worksheet("World")
+#open google sheet file named "data", the worksheet
+#named "test_world"
+#to make a new sheet in data file uncomment the following two lines
+#sheet = client.open("data")
+#sheet.add_worksheet(title="test_world", rows="100", cols="20")
+sheet = client.open("data").worksheet("test_world")
+#above line is same us the following two:
+# sheet = client.open("data")
+#sheet = sheet.worksheet("test_world")
 #get all the record on the sheet
 data = sheet.get_all_records()
 #getting the row
@@ -31,7 +46,18 @@ insert_row = ["Yae",47600]
 data2 = sheet.get_all_records()
 print(len(data2))
 sheet.insert_row(["after the last"],len(data2)+2)
-sheet.delete_row(len(data2)+1)
+sheet.delete_rows(len(data2)+1)
 data2 = sheet.get_all_records()
 sheet.insert_row(["last"],len(data2)+1)
+
+#We can now go ahead and deploy our data
+#lets scrap the and store it into a varibale df
+sheet = client.open("data").worksheet("World")
+df = Scraper("https://www.worldometers.info/coronavirus/")
+#gspread cant deploy values with NaN so we need to fix that
+df = df.replace(np.nan, '', regex=True)
+
+sheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+#DONE -  Now you can go check your worksheet
 
